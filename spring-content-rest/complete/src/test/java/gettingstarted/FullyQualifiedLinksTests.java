afaeb05293aa9806fa1b253bd6632c6817952aed
Paul Warren <paul_c_warren@yahoo.com>
2020-10-15 23:48:52 -0700

    Bump to 1.1.0.M5-SNAPSHOT
    - fixes fully qualified links issue

--- spring-content-rest/complete/src/test/java/gettingstarted/FullyQualifiedLinksTests.java
+++ spring-content-rest/complete/src/test/java/gettingstarted/FullyQualifiedLinksTests.java
@@ -1,10 +1,5 @@
 package gettingstarted;
 
-import org.springframework.test.context.TestPropertySource;
-
-@TestPropertySource(properties = {
-        "spring.content.rest.fullyQualifiedLinks=true"
-})
 public class FullyQualifiedLinksTests extends GettingStartedTest {
 
 }
